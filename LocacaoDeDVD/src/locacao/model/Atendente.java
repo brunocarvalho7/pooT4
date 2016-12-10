29103bdc2b560ce4f5b29ada2046bcbca9680b2f
Isac Moura <contatoisacmoura@outlook.com>
2016-12-09 23:37:09 -0300

    Implementações - Atendente
    Implementações no model Atendente

--- LocacaoDeDVD/src/locacao/model/Atendente.java
+++ LocacaoDeDVD/src/locacao/model/Atendente.java
@@ -1,6 +1,15 @@
 package locacao.model;
 
-public class Atendente {
+public class Atendente extends Pessoa{
+	private float salario;
+	
+	public float getSalario() {
+		return salario;
+	}
 
+	public Atendente(int idPessoa, String nome, String cpf, String endereco, String telefone, String email) {
+		super(idPessoa, nome, cpf, endereco, telefone, email);
+	}
+	
 	
 }
